a9c8957738a8e71311bb83574b5c54694c4e7eb3
Manuel Stosic <manuel.stosic@duit.de>
2013-11-05 14:39:38 +0100

    WIP: rewrite for service to use specified repository

--- src/SamVersioning/Entity/VersionedObjectRepository.php
+++ src/SamVersioning/Entity/VersionedObjectRepository.php
@@ -1,20 +1,17 @@
 <?php
-/**
- * @author    Manuel Stosic <manuel.stosic@duit.de>
- * @copyright 2013 DU-IT GmbH
- */
 namespace SamVersioning\Entity;
 
-use Doctrine\Common\Persistence\EntityRepository;
+use Doctrine\ORM\EntityRepository;
 
-class VersionedObjectRepository extends EntityRepository
+class VersionedObjectRepository extends EntityRepository implements VersionedObjectRepositoryInterface
 {
     public function getVersionsForObjectNameAndId($objectName, $objectId)
     {
-        return $this->_em->createQuery('SELECT v FROM SamVersioning\Entity\VersionedObject v WHERE v.objectName = :objectName AND v.objectId = :objectId')
-            ->setParameters(array(
-                'objectName' => $objectName,
-                'objectId'   => $objectId
-            ))->getResult();
+        return $this->findBy(array(
+            'objectName' => $objectName,
+            'objectId'   => $objectId
+        ), array(
+            'objectDate ASC'
+        ));
     }
 }
